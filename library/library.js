6ac285cb70a5bcc5ff407e5a6db27eb3f1a6f34e
cingfong <cingfong311@gmail.com>
2021-12-28 15:35:20 +0800

    library commit

---
+++ library/library.js
@@ -0,0 +1,16 @@
+function checkLayers(_val,layers){
+  let layersCount = 1
+  function check(_val){
+    Object.keys(_val).forEach(key=>{
+      if(typeof _val[key] === 'object'){
+        layersCount+=1
+        check(_val[key])
+      }
+    })
+  }
+  check(_val)
+  return layersCount === layers
+}
+
+var b = {c:1,a:{a:{a:{a:1}}}}
+console.log(checkLayers(b,3))
