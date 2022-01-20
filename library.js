698ac1de1c33f4fa8efd3ad45e65da16bc6b2aca
DESKTOP-RJBEELM\User <weiAdmin@admin>
2022-01-20 14:07:02 +0800

    test

---
+++ library.js
@@ -0,0 +1,18 @@
+// 檢查 object 共有幾層
+function checkLayers(_val, layers){
+  let layersCount = 1
+  function check(_val){
+    Object.keys(_val).forEach(key=>{
+      if(typeof _val[key] === 'object' && _val[key] !== null){
+        layersCount+=1
+        check(_val[key])
+      }
+    })
+  }
+  check(_val)
+  return layersCount === layers
+}
+
+var b = {c:1,a:{a:{a:{a:1}}}}
+//test
+console.log(checkLayers(b,3))
