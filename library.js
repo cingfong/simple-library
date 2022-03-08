d965a0c092cd1e4e274259152aea9ba26bb26bf8
cingfong <cingfong311@gmail.com>
2022-03-08 16:15:07 +0800

    t

--- library.js
+++ library.js
@@ -16,3 +16,31 @@ function checkLayers(_val, layers){
 var b = {c:1,a:{a:{a:{a:1}}}}
 // test
 console.log(checkLayers(b,3))
+
+// 深拷貝
+const a = {
+  name: '張三',
+  children: [
+    {son: '小名'},
+    {daughter: '小美'}
+  ]
+}
+
+function clone(obj){
+  let _obj = Array.isArray(obj) ? [] : {};
+  if(obj && typeof obj === "object"){
+      for(key in obj){
+          if(obj.hasOwnProperty(key)){
+              // 判断obj子元素是否为对象，如果是，递归复制
+              if(obj[key] && typeof obj[key] === "object"){
+                  _obj[key] = clone(obj[key]);
+              } else {
+                  // 如果不是，简单复制
+                  _obj[key] = obj[key];
+              }
+          }
+      }
+  }
+  return _obj;
+};
+const c = clone(a);
